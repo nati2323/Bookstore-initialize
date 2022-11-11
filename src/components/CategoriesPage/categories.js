e8f4a4386294a58234601b133fd65a2c66884071
Natnael Amare <99729850+nati2323@users.noreply.github.com>
2022-11-11 13:51:36 +0300

    Update categories.js

--- src/components/CategoriesPage/categories.js
+++ src/components/CategoriesPage/categories.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
 const Category = () => {
- return (
-    <button type="button">Check Status</button>
-  );
+ return <button type="button">Check Status</button>
 };
 
 export default Category;
