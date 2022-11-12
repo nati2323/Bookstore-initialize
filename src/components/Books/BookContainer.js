c5c5e2c0dc39e21cfa04f2be6b3dde2a8c18ee05
nati2323 <nati2.man.3@gmail.com>
2022-11-12 18:31:49 +0300

    fix linters error

--- src/components/Books/BookContainer.js
+++ src/components/Books/BookContainer.js
@@ -3,10 +3,10 @@ import BooksList from './BooksList';
 import AddBook from './AddBook';
 
 const Books = () => {
-  let books = [ { title: 'bears trap', author: 'hamid gul' } ,
-                { title: 'confessions', author: 'Agatha christie' }
- ];
-  const BookList = books.map ( book => {
+  const books = [{ title: 'bears trap', author: 'hamid gul' },
+ { title: 'confessions', author: 'Agatha christie' },
+  ];
+  const BookList = books.map (book => {
            return (<BooksList title={ book.title } author = { book.author } />)
   });
   return (
